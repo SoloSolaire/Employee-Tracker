46c4c530afe7edda863a0927cafae990f25330e3
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-09-04 14:09:35 -0700

    debugged schema syntax

--- db/schema.sql
+++ db/schema.sql
@@ -4,25 +4,25 @@ CREATE DATABASE tracker_db;
 USE tracker_db;
 
 CREATE TABLE department (
-    id INT NOT NULL PRIMARY KEY,
-    name NOT NULL VARCHAR(30)
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY,
-    title VARCHAR(30)NOT NULL ,
-    salary DECIMAL NOT NULL ,
-    department_id INT NOT NULL ,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30)NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL ,
-    last_name VARCHAR(30) NOT NULL ,
-    role_id INT NOT NULL ,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
